bb1872d7e63197e3ef6aac09675c84872d2f8789
kirbycbressler <41982722+kirbycbressler@users.noreply.github.com>
2021-06-28 18:30:29 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css'
+import NavBar from './components/navBar'
+import Footer from './components/footer'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import Catalog from './components/catalog'
+function App () {
+  return (
+    <div className='App'>
+      <NavBar></NavBar>
+      <Catalog></Catalog>
+      <Footer></Footer>
+    </div>
+  )
+}
+
+export default App;
