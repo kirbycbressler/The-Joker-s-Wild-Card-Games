3ca8573682fca755819710b5b67ccad22de0460c
kirbycbressler <41982722+kirbycbressler@users.noreply.github.com>
2021-08-01 15:16:38 -0700

    Add files via upload

--- services/itemServices.js
+++ services/itemServices.js
@@ -1,60 +1,63 @@
 import axios from 'axios'
-var data = [
-  {
-    _id: '0',
-    title: 'UNO',
-    description: 'One man army, You know?',
-    image: 'uno-1.jpg',
-    price: 11.17,
-    category: 'Family',
-    discount: 10
-  },
-  {
-    _id: '011',
-    title: 'Hand And Foot',
-    description: 'Head, Shoulders Hands and feet, Hands And Feet',
-    image: 'Cards.jpg',
-    price: 11.15,
-    category: 'Family',
-    discount: 10
-  },
-  {
-    _id: '012',
-    title: 'Crazy 8s',
-    description: 'This one is for The WILD Ones',
-    image: 'cards2.png',
-    price: 11.14,
-    category: 'Kids',
-    discount: 10
-  },
-  {
-    _id: '0123',
-    title: 'BlackJack',
-    description: 'House wins "everytime"',
-    image: 'cards3.png',
-    price: 11.13,
-    category: 'Friends',
-    discount: 10
-  },
-  {
-    _id: '0111',
-    title: 'Go Fish',
-    description: 'Dont lie, or do lie, either way, you better let the kid win.',
-    image: 'cards1.png',
-    price: 11.12,
-    category: 'Kids',
-    discount: 10
-  },
-  {
-    _id: '01',
-    title: 'Poker',
-    description: 'Poker face',
-    image: 'poker-1.jpg',
-    price: 11.16,
-    category: 'Friends',
-    discount: 10
-  }
-]
+
+// use admin to bring over data
+
+// var data = [
+//   {
+//     _id: '0',
+//     title: 'UNO',
+//     description: 'One man army, You know?',
+//     image: 'uno-1.jpg',
+//     price: 11.17,
+//     category: 'Family',
+//     discount: 10
+//   },
+//   {
+//     _id: '011',
+//     title: 'Hand And Foot',
+//     description: 'Head, Shoulders Hands and feet, Hands And Feet',
+//     image: 'Cards.jpg',
+//     price: 11.15,
+//     category: 'Family',
+//     discount: 10
+//   },
+//   {
+//     _id: '012',
+//     title: 'Crazy 8s',
+//     description: 'This one is for The WILD Ones',
+//     image: 'cards2.png',
+//     price: 11.14,
+//     category: 'Kids',
+//     discount: 10
+//   },
+//   {
+//     _id: '0123',
+//     title: 'BlackJack',
+//     description: 'House wins "everytime"',
+//     image: 'cards3.png',
+//     price: 11.13,
+//     category: 'Friends',
+//     discount: 10
+//   },
+//   {
+//     _id: '0111',
+//     title: 'Go Fish',
+//     description: 'Dont lie, or do lie, either way, you better let the kid win.',
+//     image: 'cards1.png',
+//     price: 11.12,
+//     category: 'Kids',
+//     discount: 10
+//   },
+//   {
+//     _id: '01',
+//     title: 'Poker',
+//     description: 'Poker face',
+//     image: 'poker-1.jpg',
+//     price: 11.16,
+//     category: 'Friends',
+//     discount: 10
+//   }
+// ]
 
 class ItemService {
   // methods to get service
@@ -63,12 +66,16 @@ class ItemService {
     // logic to connect to a server and retrieve the list items
     // URL: http://127.0.0.1:5000/api/catalog  GET
     let response = await axios.get('http://127.0.0.1:5000/api/catalog')
+
     return response.data
 
     // return mock data
     // return data
   }
-
+  async placeOrder (order) {
+    let response = await axios.post('http://127.0.0.1:5000/api/order', order)
+    return response.data
+  }
   // method to save a product on the server
   // http://127.0.0.1:5000/api/catalog
   async saveItem (item) {
@@ -86,8 +93,15 @@ class ItemService {
     let response = await axios.get('http://127.0.0.1:5000/api/categories')
     return response.data
   }
+  async validateCode (code) {
+    let response = await axios.get(
+      'http://127.0.0.1:5000/api/discountCode/' + code
+    )
+    return response.data
+  }
 
   getStock (itemId) {}
+
   getItemPrice (cupon) {}
 }
 
